6b73c49f0177cde4474422ec3499f64381e554bc
Smokey <kmboplays@gmail.com>
2021-10-12 16:42:14 +0500

    added rest of stuff

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,15 @@
-require('./bootstrap');
+require("./bootstrap");
 
-require('alpinejs');
+require("alpinejs");
+
+import { createApp } from "vue";
+import router from "./router";
+import CompaniesIndex from "./components/companies/CompaniesIndex";
+
+createApp({
+    components: {
+        CompaniesIndex,
+    },
+})
+    .use(router)
+    .mount("#app");
