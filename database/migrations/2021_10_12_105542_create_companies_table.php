fdd4f814589990e4c81a8288cf0f7134dcb6fb98
Smokey <kmboplays@gmail.com>
2021-10-12 16:11:14 +0500

    updated some company resource

--- database/migrations/2021_10_12_105542_create_companies_table.php
+++ database/migrations/2021_10_12_105542_create_companies_table.php
@@ -15,6 +15,10 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('website')->nullable();
             $table->timestamps();
         });
     }
